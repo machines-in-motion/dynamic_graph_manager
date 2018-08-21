2ab96caa1ca46e2d2c5b51205c01ce2c93b51e63
Naveau <maximilien.naveau@gmail.com>
2018-08-21 18:16:20 +0200

    Update the packaging to the code
    add the tools.cpp file

--- cmake/pkg-config.cmake
+++ cmake/pkg-config.cmake
@@ -13,8 +13,6 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-INCLUDE(cmake/shared-library.cmake)
-
 FIND_PACKAGE(PkgConfig)
 
 # Additional pkg-config variables whose value will be imported
