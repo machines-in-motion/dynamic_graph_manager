eec78adfa93e15f5ae105902c139ade61457b458
Maximilien Naveau <maximilien.naveau@tuebingen.mpg.de>
2019-04-17 11:40:19 +0200

    fix the catkin_make install

--- setup.py
+++ setup.py
@@ -2,8 +2,6 @@ from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
 # fetch values from package.xml
-setup_args = generate_distutils_setup(
-  scripts=['python/ros_nodes'],
-)
+setup_args = generate_distutils_setup()
 
 setup(**setup_args)
